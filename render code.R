998798085784f80c99960cc27a22b7adaf62b1c5
Zichang23 <zichang23@gmail.com>
2021-07-21 17:27:02 -0400

    I've change the folder name.

---
+++ render code.R
@@ -0,0 +1,6 @@
+utils::download.file("https://github.com/Zichang23/Project-3/archive/master.tar.gz", method = "libcurl")
+shiny::runGitHub('Project-3', 'Zichang23', subdir = "Rroject-3", ref = "main")
+
+shiny::runGitHub('shiny_example', 'rstudio')
+
+shiny::runGitHub("shiny-examples", "rstudio", subdir = "001-hello")
